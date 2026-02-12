92b71c59605e230f1387f0959bdf973c43ee5cbd
Alexander Saadia <asaadiajinich@lcs.on.ca>
2026-02-12 09:54:29 -0500

    Initial Commit

---
+++ GeometricFigures/GeometricFiguresApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricFiguresApp.swift
+//  GeometricFigures
+//
+//  Created by Alexander Saadia on 12/02/26.
+//
+
+import SwiftUI
+
+@main
+struct GeometricFiguresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
