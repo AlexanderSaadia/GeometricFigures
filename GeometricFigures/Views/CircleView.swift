1cae16cc3d7b0f328f24722ea14e4e268e39c241
Alexander Saadia <asaadiajinich@lcs.on.ca>
2026-02-12 10:14:47 -0500

    I have a model, I have a view, I slider changes data, I UI updates, and I have a radius.

--- GeometricFigures/Views/CircleView.swift
+++ GeometricFigures/Views/CircleView.swift
@@ -1,10 +1,34 @@
 // A structure that conforms to View must have a computed property named body that returns one view.
 
+// @State means: “This value may change. When it changes, update the UI.”
+
+// $ means: “This control is connected to that stored property.”
+
+
 import SwiftUI
 
 struct CircleView: View {
+    
+    @State var currentCircle = Circle(radius: 10)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // TODO: Add image later
+         
+            // Label for the slider
+            Text("Radius")
+            
+            // Slider for radius input
+            Slider(
+                value: $currentCircle.radius,
+                in: 1...100,
+                step: 1.0
+            )
+            
+            // Label showing the current slider value
+            Text("Radius is \(currentCircle.radius.formatted())")
+            
+        }
     }
 }
 
